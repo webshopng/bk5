333f9f908c71fb4514add72af7cdfb62e3f7b33a
Babatunde <85345191+webshopng@users.noreply.github.com>
2025-03-07 22:57:19 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,15 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          id="google-adsense"
-          async
-          strategy="afterInteractive"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8647586631110951"
-          crossOrigin="anonymous"
-        />
-      </head>
+      <Script
+        id="google-adsense"
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8647586631110951"
+        crossOrigin="anonymous"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   )
